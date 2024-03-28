a61604cff50f4edb324f4ac533927a4a17c13e6c
Andre Luckyta Firdaus <andreluckytafirdaus2001@gmail.com>
2024-03-28 11:49:24 +0700

    Commit Revisi (dummy)

--- resources/views/siswa/simulasi.blade.php
+++ resources/views/siswa/simulasi.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="content w-75">
-    <h2 class="text-left mb-lg-5" style="color: #0F3077">Simulasi SKD</h2>
+    <h2 class="text-left mb-lg-5" style="color: #0F3077">Latihan SKD</h2>
     <div class="d-flex align-items-center flex-column mb-3">
         <div class="p-2">
             <a href="/siswa/simulasi/TKP">
@@ -11,12 +11,12 @@
         </div>
         <div class="p-2">
             <a href="/siswa/simulasi/TIU">
-                <img src="{{ asset('assets/TIU.svg') }} "alt="TIU">
+                <img src="{{ asset('assets/TWK.svg') }} "alt="TIU">
             </a>
         </div>
         <div class="p-2">
             <a href="/siswa/simulasi/TWK">
-                <img src="{{ asset('assets/TWK.svg') }} "alt="TWK">
+                <img src="{{ asset('assets/TIU.svg') }} "alt="TWK">
             </a>
         </div>
     </div>
