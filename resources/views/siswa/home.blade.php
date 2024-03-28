cac22fc8290fd274f50352b8dfe0273fc337d42a
Andre Luckyta Firdaus <andreluckytafirdaus2001@gmail.com>
2024-03-28 20:53:27 +0700

    Konsultasi dan home

--- resources/views/siswa/home.blade.php
+++ resources/views/siswa/home.blade.php
@@ -3,6 +3,9 @@
 @section('background')
     <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-inner">
+           
+        <div class="image-container">
+
             <div class="carousel-item active">
                 <img src="{{ asset('assets/background.png') }}" class="d-block w-100" alt="Background Image 1">
             </div>
@@ -12,6 +15,8 @@
             <div class="carousel-item">
                 <img src="{{ asset('assets/background3.jpeg') }}" class="d-block w-100 img-opacity" alt="Background Image 3">
             </div>
+            </div>
+
         </div>
         <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -24,19 +29,32 @@
     </div>
 <style>
     .img-opacity {
-        opacity: 0.65; /* opacity / transparation */
+        opacity: 0.55; /* opacity / transparation */
     }
+
+    .image-container {
+        width: 2500px; /* Set container width */
+        height: 2000px; /* Set container height */
+        overflow: hidden; /* Ensure image doesn't overflow */
+    }
+
+    .image-container img {
+        width: 100%; /* Make image fill container */
+        height: 100%; /* Make image fill container */
+        object-fit: cover; /* Maintain aspect ratio and cover entire container */
+    }
+
 </style>
 
 @endsection
 
 @section('content')
-    <div class="content mt-5">
+    <div class="content mt-5" style ="padding-top: 250px; position: absolute;">
         <div class="row align-items-center">
-            <div class="col-md-2 col-4"> <!-- Ubah menjadi 4 kolom pada perangkat seluler -->
+            <div class="col-md-2 "> 
                 <img src="{{ asset('assets/logo.png') }}" alt="Logo" style="width: 100px; height: 100px" class="img-fluid">
             </div>
-            <div class="col-md-10 col-8"> <!-- Ubah menjadi 8 kolom pada perangkat seluler -->
+            <div class="col-md-10 "> 
             <h1 class="display-4 text-start fw-bold outlined-text" style="color: #5DB6FA; text-shadow: 2px 2px 4px #000000;">Bimbingan Belajar</h1>
             <p class="lead text-start fw-bold display-6" style="color: #0F3077">Sinar Prestasi</p>
             </div>
@@ -44,20 +62,20 @@
 
     <div class="row mt-4">
         <div class="col-md-12">
-        <p class="text-left" style="color: rgba(0, 0, 0, 1); font-size: 1.5rem;"> <!-- Menambahkan properti font-size: 1.2rem; untuk memperbesar tulisan -->
+        <p class="text-left" style="color: rgba(0, 0, 0, 1); font-size: 1.1rem; text-align: justify; "> <!-- Menambahkan properti font-size: 1.2rem; untuk memperbesar tulisan -->
             Bimbingan Belajar Sinar Prestasi merupakan lembaga pendidikan yang secara khusus untuk persiapan seleksi
             masuk sekolah kedinasan. Adapun sekolah kedinasan yang diampu adalah sekolah kedinasan yang menjadikan
             Seleksi Kompetensi Dasar atau Computer Assisted Test sebagai salah satu materi seleksi yang diujikan yaitu
             Test Wawasan Kebangsaan, Tes Intelejensi Umum, dan Tes Karakteristik Pribadi.
         </p>
-        <p class="text-left" style="color: rgba(0, 0, 0, 1); font-size: 1.5rem;"> <!-- Sama seperti di atas, menambahkan properti font-size: 1.2rem; untuk memperbesar tulisan -->
+        <p class="text-left" style="color: rgba(0, 0, 0, 1); font-size: 1.1rem;text-align: justify;"> <!-- Sama seperti di atas, menambahkan properti font-size: 1.2rem; untuk memperbesar tulisan -->
             Berbeda dengan lembaga pendidikan lain yang hanya menyajikan akademik sebagai bahan ajar utamanya,
             Bimbingan Belajar Sinar Prestasi mebimbing siswa dan siswi dari mulau SKD/TKD, Konsultasi Kesehatan,
             Psikologi, dan Kesempatan. Maka, Bimbingan Sinar Prestasi menjadikan One Stop Preparation Course
             yang memadukan bimbingan untuk semua jenis tes yang diujikan dengan standar kompetensi yang sesuai
             dengan kebutuhan.
         </p>
-        <p class="text-left" style="color: rgba(0, 0, 0, 1); font-size: 1.5rem;"> <!-- Sama seperti di atas, menambahkan properti font-size: 1.2rem; untuk memperbesar tulisan -->
+        <p class="text-left" style="color: rgba(0, 0, 0, 1); font-size: 1.1rem;text-align: justify;"> <!-- Sama seperti di atas, menambahkan properti font-size: 1.2rem; untuk memperbesar tulisan -->
             Untuk mendukung proses pelaksanaan belajar dan mengajar agar maksimal, Bimbingan Belajar Sinar Prestasi
             telah menyediakan asrama, dengan adanya asrama menjadikan siswa dan siswa lebih fokus dalam latihan dan belajar.
             Lalu, Bimbingan Belajar Sinar Prestasi dilengkapi degan metode pembelajaran yang modern serta pengajar yang
--- resources/views/siswa/konsultasi.blade.php
+++ resources/views/siswa/konsultasi.blade.php
@@ -13,7 +13,7 @@
 @endsection
 
 @section('content')
-    <div class="content w-50">
+    <div class="content w-50" style ="padding-top: 125px;">
         <div class="card rounded-5 mt-3 py-2">
             <div class="card-body px-5 py-3">
                 <div class="row justify-content-center">
@@ -23,28 +23,28 @@
                 </div>
 
                 <div>
-                    <button type="button" class="form-control btn btn-lg rounded-pill px-4 my-4 w-75" style="background-color: #0F3077; color:#DFF8FD">
+                    <button type="button" class="form-control btn btn-lg rounded-pill px-2 my-2 w-75" style="background-color: #0F3077; color:#DFF8FD">
                         <i class="fas fa-phone mr-2"></i>
                         +62 822-9556-2910 
                     </button>
-                    <button type="button" class="form-control btn btn-lg rounded-pill px-4 w-75" style="background-color: #0F3077; color:#DFF8FD">
-                        <i class="fas fa-envelope mr-2"></i> <a href="mailto:bimbelsinarprestasi@gmail.com">Send Email</a>
-                        bimbelsinarprestasi@gmail.com
+                    <button type="button" class="form-control btn btn-lg rounded-pill px-2 w-75" style="background-color: #0F3077; color:#DFF8FD ">
+                        <i class="fas fa-envelope mr-2"></i> <a href="mailto:bimbelsinarprestasi@gmail.com"> Bimbelsinarprestasi@gmail.com</a>
+                        
                     </button>
                     
-                    <button type="button" class="form-control btn btn-lg rounded-pill px-4 my-4 w-75" style="background-color: #60D669; color: #000000;">
-                        <i class="fab fa-whatsapp fa-bounce mr-2"></i><a href="https://api.whatsapp.com/send?phone082295562910&text=Halo%20kak%20,%20Saya%20tertarik%20buat%20join%20bimbelnya%20kak.">Send Message</a>
-                        Chat on Whatsapp
+                    <button type="button" class="form-control btn btn-lg rounded-pill px-2 my-2 w-75" style="background-color: #60D669; color: #000000;border: 2px solid black ;">
+                        <i class="fab fa-whatsapp fa-bounce mr-2"></i><a href="https://api.whatsapp.com/send?phone082295562910&text=Halo%20kak%20,%20Saya%20tertarik%20buat%20join%20bimbelnya%20kak."> Send Message</a>
+                        
                     </button>
                     
-                    <button type="button" class="form-control btn btn-lg rounded-pill px-4 my-4 w-75" style="background-color: #ffffff; color: #ff00dd;">
-                        <i class="fa-brands fa-instagram fa-bounce mr-2" ></i><a href="https://www.instagram.com/bimbelsinarprestasi/.">Instagram</a>
-                        Check Our Instagram
+                    <button type="button" class="form-control btn btn-lg rounded-pill px-2 my-2 w-75" style="background-color: #ffffff; color: #ff00ae; border: 2px solid black ;">
+                    <i class="fab fa-instagram mr-2"></i><a href="https://www.instagram.com/bimbelsinarprestasi/."> Check Our Instagram</a>
                     </button>
 
-                    <button type="button" class="form-control btn btn-lg rounded-pill px-4 my-4 w-75" style="background-color: #ffffff; color: #0092ff;">
-                        <i class="fab fa-facebook-f fa-shake mr-2"></i><a href="https://www.facebook.com/profile.php?id=61557157866649.">Facebook</a>
-                        Check Our Facebook
+
+                    <button type="button" class="form-control btn btn-lg rounded-pill px-4 my-4 w-75" style="background-color: #ffffff; color: #0092ff; border: 2px solid black ;">
+                        <i class="fab fa-facebook-f fa-shake mr-4"></i><a href="https://www.facebook.com/profile.php?id=61557157866649."> Check Our Facebook</a>
+                        
                     </button>
 
                 </div>
